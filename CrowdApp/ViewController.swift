908ff5d2b6c127b04d65780108ef7bfceefb0f5c
Daniel Andersen <daniel.andersen@alexandra.dk>
2014-11-24 10:23:54 +0100

    Initial Commit

---
+++ CrowdApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrowdApp
+//
+//  Created by Daniel Andersen on 24/11/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
